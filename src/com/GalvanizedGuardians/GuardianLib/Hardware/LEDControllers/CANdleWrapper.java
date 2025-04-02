cc3327696e01dc572e7573ca43946664e96570c6
JesseW2158 <jessewang2158@gmail.com>
2025-04-01 22:21:02 -0400

    Some More Fixes + LEDController

--- src/com/GalvanizedGuardians/GuardianLib/Hardware/LEDControllers/CANdleWrapper.java
+++ src/com/GalvanizedGuardians/GuardianLib/Hardware/LEDControllers/CANdleWrapper.java
@@ -1,7 +1,52 @@
-/* Copyright (c) 2025 Galvanized Robotics Robotics. All rights reserved. */
+/* Copyright (c) 2025 StuyPulse Robotics. All rights reserved. */
 /* This work is licensed under the terms of the MIT license */
 /* found in the root directory of this project. */
 
 package com.GalvanizedGuardians.GuardianLib.Hardware.LEDControllers;
 
-public class CANdleWrapper {}
+import com.GalvanizedGuardians.GuardianLib.Hardware.LEDControllers.Utility.CANDeviceDetails;
+import com.GalvanizedGuardians.GuardianLib.Logging.Faults.CANdleFaultsWrapper;
+
+import com.ctre.phoenix.led.CANdle;
+
+public class CANdleWrapper implements LEDControllerIO {
+    private CANdle leds;
+    private CANdleState currState;
+    private CANdleFaultsWrapper faults;
+    private CANDeviceDetails details;
+    private int ledCount;
+
+    
+    public enum CANdleState {
+        OFF,
+        TURNING_CW,
+        TURNING_CCW,
+        RESET_POSE,
+        RESET_GYRO,
+        ALIGNING,
+        ALIGNED
+    }
+
+    public enum Animations {
+        ColorFlow,
+        Fire,
+        Larson,
+        Rainbow,
+        RgbFade,
+        SingleFade,
+        Strobe,
+        Twinkle,
+        TwinkleOff,
+        SetAll,
+        Empty
+    }
+
+    public class CANdleIOInputs {
+        public CANdleState state = CANdleState.OFF;
+    }
+
+    public CANdleWrapper(CANDeviceDetails details, int ledCount, String CANBus) {
+        this.details = details;
+        
+    }
+}
--- src/com/GalvanizedGuardians/GuardianLib/Hardware/LEDControllers/LEDControllerIO.java
+++ src/com/GalvanizedGuardians/GuardianLib/Hardware/LEDControllers/LEDControllerIO.java
@@ -1,7 +1,26 @@
-/* Copyright (c) 2025 Galvanized Robotics Robotics. All rights reserved. */
+/* Copyright (c) 2025 StuyPulse Robotics. All rights reserved. */
 /* This work is licensed under the terms of the MIT license */
 /* found in the root directory of this project. */
 
 package com.GalvanizedGuardians.GuardianLib.Hardware.LEDControllers;
 
-public class LEDControllerIO {}
+import com.GalvanizedGuardians.GuardianLib.Hardware.LEDControllers.CANdleWrapper.CANdleState;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface LEDControllerIO {
+    @AutoLog
+    public class LEDControllerIOInputs {
+        public CANdleState state = CANdleState.OFF;
+    }
+
+    public default void updateInputs(LEDControllerIOInputs inputs) {}
+
+    public default void setLEDS(CANdleState state) {}
+
+    public default void setLEDS(CANdleState state, double seconds) {}
+
+    public default void periodic() {}
+
+    public default void checkForFaults() {}
+}
