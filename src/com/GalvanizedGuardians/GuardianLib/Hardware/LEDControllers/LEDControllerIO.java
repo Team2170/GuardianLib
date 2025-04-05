21a64d1108bb5314e26337ace7aa0eed51143539
JesseW2158 <jessewang2158@gmail.com>
2025-04-05 19:17:37 -0400

    Jitpack Implementation Instead

--- src/com/GalvanizedGuardians/GuardianLib/Hardware/LEDControllers/LEDControllerIO.java
+++ src/com/GalvanizedGuardians/GuardianLib/Hardware/LEDControllers/LEDControllerIO.java
@@ -1,25 +1,26 @@
-/* Copyright (c) 2025 StuyPulse Robotics. All rights reserved. */
+/* Copyright (c) 2025 GalvanizedGuardians Robotics. All rights reserved. */
 /* This work is licensed under the terms of the MIT license */
 /* found in the root directory of this project. */
 
 package com.GalvanizedGuardians.GuardianLib.Hardware.LEDControllers;
 
 import com.GalvanizedGuardians.GuardianLib.Hardware.LEDControllers.CANdleWrapper.CANdleState;
+
 import org.littletonrobotics.junction.AutoLog;
 
 public interface LEDControllerIO {
-  @AutoLog
-  public class LEDControllerIOInputs {
-    public CANdleState state = CANdleState.OFF;
-  }
+    @AutoLog
+    public class LEDControllerIOInputs {
+        public CANdleState state = CANdleState.OFF;
+    }
 
-  public default void updateInputs(LEDControllerIOInputs inputs) {}
+    public default void updateInputs(LEDControllerIOInputs inputs) {}
 
-  public default void setLEDS(CANdleState state) {}
+    public default void setLEDS(CANdleState state) {}
 
-  public default void setLEDS(CANdleState state, double seconds) {}
+    public default void setLEDS(CANdleState state, double seconds) {}
 
-  public default void periodic() {}
+    public default void periodic() {}
 
-  public default void checkForFaults() {}
+    public default void checkForFaults() {}
 }
